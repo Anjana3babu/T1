2fff3be25a7a81d91d478caaccdcfd335bc1f2c9
Anjana3babu <anjanababubeemanadi@gmail.com>
2025-03-26 21:54:32 +0530

    Page publish

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    host: '0.0.0.0', // Allows access from any device
-    port: 3000, // Ensure this matches your ngrok port
-    strictPort: true, 
-    allowedHosts: ['.ngrok-free.app'], // Allow all ngrok subdomains
-  },
+  // server: {
+  //   host: '0.0.0.0', // Allows access from any device
+  //   port: 3000, // Ensure this matches your ngrok port
+  //   strictPort: true, 
+  //   allowedHosts: ['.ngrok-free.app'], // Allow all ngrok subdomains
+  // },
 
   plugins: [react()],
+  base: "/T1/",
+
 })
